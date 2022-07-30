af05ccf34dbde019a697c0e973f13587a7a0e592
Camden Smith <camdensmithh@gmail.com>
2022-07-29 23:11:46 -0700

    Server connection

--- server/index.js
+++ server/index.js
@@ -1,19 +1,12 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const db = require('./db/schema');
+const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost/3000')
-  .then(() => {
-    console.log('Success connecting mongoose (server/index.js)');
-  })
-  .catch((error) => {
-    console.log('Error connecting mongoose (server/index.js): ', error);
-  });
+app.use('/qa', routes);
 
 app.listen(3000, () => {
-  console.log('Server is running at port 3000');
+  console.log(`Server is running at port ${3000}`);
 });
