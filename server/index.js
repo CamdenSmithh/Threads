72562a4155102332ad0bd88962048c7e4aeaac16
Camden Smith <camdensmithh@gmail.com>
2022-08-01 09:36:17 -0700

    Added Cors

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/qa', routes);
 
-app.listen(3000, () => {
-  console.log(`Server is running at port ${3000}`);
+app.listen(3001, () => {
+  console.log(`Server is running at port ${3001}`);
 });
